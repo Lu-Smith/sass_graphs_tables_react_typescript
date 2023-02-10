be7b77a9466a06cfd68109cae7712629d2d063fc
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-02-10 14:59:43 +0000

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer: React.FC = () => {
   return (
-    <div>Footer</div>
+    <div className='Footer'>
+        <h3>Coded by <a href='https://www.lunasmithart.com/' target="_blank" rel="noreferrer">Luna Smith</a>, open-source on <a href='https://github.com/Lu-Smith/sass_graphs_tables_react_typescript' target="_blank" rel="noreferrer">GitHub</a>.</h3>
+    </div>
   )
 }
 
