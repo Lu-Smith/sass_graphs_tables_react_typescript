03166b274c59601014de2657c61be0ccc1a38637
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-02-10 14:56:42 +0000

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Header: React.FC = () => {
   return (
-    <div>Header</div>
+    <div className='Header'>
+        <h1>Fun with graphs and tables with SASS</h1>
+        <ul>
+            <li>
+                GRAPHS
+            </li>
+            <li>
+                TABLES
+            </li>
+        </ul>
+    </div>
   )
 }
 
