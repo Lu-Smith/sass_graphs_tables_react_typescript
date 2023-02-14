39ca53b4368aa98e5d6aa0084c1a3699151dc145
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-02-14 23:39:00 +0000

    Updated Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 const Main: React.FC = () => {
+  const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    fetch('http://substantiveresearch.pythonanywhere.com/')
+      .then(response => response.json())
+      .then(data => setData(data));
+      console.log(data);
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>Main</div>
+    <div className='Main'>
+      <h2>Graph 1</h2>
+    </div>
   )
 }
 
