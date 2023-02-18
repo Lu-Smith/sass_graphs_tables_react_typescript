a86c79003a7b5f269805c97a1d295e749f85ca8f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-02-18 23:16:05 +0000

    Added animation to link in Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer: React.FC = () => {
   return (
     <div className='Footer'>
-        <h3>Coded by <a href='https://www.lunasmithart.com/' target="_blank" rel="noreferrer" className="wave-link">Luna Smith</a>, open-source on <a href='https://github.com/Lu-Smith/sass_graphs_tables_react_typescript' target="_blank" rel="noreferrer">GitHub</a>.</h3>
+        <h3>Coded by <a href='https://www.lunasmithart.com/' target="_blank" rel="noreferrer" className="wave-link">Luna Smith</a>, open-source on <a href='https://github.com/Lu-Smith/sass_graphs_tables_react_typescript' target="_blank" rel="noreferrer" className="wave-link">GitHub</a>.</h3>
     </div>
   )
 }
